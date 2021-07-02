7274ad9d1ee88d9de674d0ba0373c3fabd2ba37e
franklinr <franklinr@users.noreply.github.com>
2021-07-02 22:48:19 +0900

    Add files via upload

---
+++ BonjourMirrorIOS/Assets/BonjourDiscovery.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+using System.Net;
+using Mirror;
+using UnityEngine.UI;
+using System.Net.Sockets;
+using System;
+
+public class BonjourDiscovery : MonoBehaviour
+{
+
+    public Text statusout, clientText;
+    public Button ServerButton, ClientButton;
+    bool querying = false;
+
+    string label;
+    string status, broadcastStatus = "no broadcast";
+
+    string service = "_tictactoe._tcp";
+
+    string[] services = new System.String[0];
+    string[] oldservices = new System.String[0];
+
+    public MyNetworkManager manager;
+
+    GUIStyle labelStyle;
+
+
+    void Start()
+    {
+        // Screen.SetResolution(640, 480, true);
+        clientText = ClientButton.GetComponent<Text>();
+    }
+
+
+
+    public void StartBroadcastService()
+    {
+        string add = GetLocalIPAddressSockets();
+
+        // Start lookup for specified service inside "local" domain
+        Bonjour.StartService(service + ".", add, 5000, "local");
+
+        broadcastStatus = "broad " + add;
+        statusout.text = statusout.text + "\nbroad " + add;
+        manager.StartHost();
+    }
+
+    public void ToggleLookup()
+    {
+        if (querying)
+        {
+            Debug.Log("Stop lookup");
+            Bonjour.StopLookup();
+            clientText.text = "Query";
+            statusout.text = statusout.text + "\nstop";
+        }
+        else
+        {
+            // Start lookup for specified service inside "local" domain
+            Debug.Log("Start lookup");
+            Bonjour.StartLookup(service, "local.");
+            querying = true;
+            status = "";
+            statusout.text = statusout.text + "\nquery ";
+
+            clientText.text = "Stop";
+        }
+
+    }
+
+    void ReportServiceConnections()
+    {
+        // List of looked up services
+        for (int i = 0; i < services.Length; i++)
+        {
+            // check if this service has been reported before
+            bool old = false;
+            for (int j = 0; j < oldservices.Length; j++)
+            {
+                if (oldservices[j] == services[i]) old = true;
+                Debug.Log(oldservices[j]);
+            }
+
+
+            if (services[i] != GetLocalIPAddressSockets() && !old)
+            {
+                manager.networkAddress = services[i];
+
+                if (manager.networkAddress != "localhost")
+                {
+                    statusout.text = statusout.text + "\nstart client " + manager.networkAddress;
+
+                    Debug.Log("start client " + manager.networkAddress);
+                    manager.StartClient();
+                }
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (querying)
+        {
+            if (Time.frameCount % 10 == 0)
+            {
+                status = Bonjour.GetLookupStatus();
+                oldservices = services;
+                services = Bonjour.GetServiceNames();
+                label = status;
+            }
+
+            if (status == "Done")
+                querying = false;
+        }
+
+        ReportServiceConnections();
+    }
+
+
+    //// Different ways to get your ip address
+
+    public static string GetLocalIPAddressSockets()
+    {
+        string localIP = string.Empty;
+        using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
+        {
+            socket.Connect("8.8.8.8", 65530);
+            IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+            localIP = endPoint.Address.ToString();
+        }
+        Console.WriteLine("IP Address = " + localIP);
+        return (localIP);
+    }
+
+    public static string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            Debug.Log("ip " + ip.ToString());
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+                return ip.ToString();
+        }
+        throw new Exception("No network adapters with an IPv4 address in the system!");
+    }
+
+    public string GetLocalIPv4()
+    {
+        return Dns.GetHostEntry(Dns.GetHostName()).AddressList.GetValue(0).ToString();
+    }
+
+
+
+}
+
---
+++ BonjourMirrorIOS/Assets/BonjourTest.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Net;
+using Mirror;
+
+public class BonjourTest : MonoBehaviour
+{
+
+    bool querying = false;
+
+    string label;
+    string status, broadcastStatus = "no broadcast";
+    // Default service name. _http._tcp corresponds to http services (f.e. printers)
+    // string service = "_http._tcp.";
+    string service = "_tictactoe._tcp";
+
+    string[] services = new System.String[0];
+
+    int centerX = 4 * Screen.width / 5;
+    int buttonHeight = 70;
+    int fontsize = 30;
+    int startY = 140;
+    public NetworkManager manager;
+
+    GUIStyle labelStyle;
+
+
+    void Start()
+    {
+        // Screen.SetResolution(640, 480, true);
+    }
+
+    public string GetLocalIPv4()
+    {
+        return Dns.GetHostEntry(Dns.GetHostName()).AddressList.GetValue(0).ToString();
+    }
+
+    void StartBroadcastService()
+    {
+        IOSNetworkPermission.TriggerDialog();
+        string add = GetLocalIPv4();
+        // Start lookup for specified service inside "local" domain
+        Bonjour.StartService(service + ".", add, 47777, "local");
+
+        broadcastStatus = "broad " + add;
+
+        manager.StartHost();
+    }
+
+    void StartLookup()
+    {
+        // Start lookup for specified service inside "local" domain
+        Bonjour.StartLookup(service, "local.");
+        querying = true;
+        status = "";
+
+
+    }
+
+    void OnGUI()
+    {
+
+
+        // GUI.skin.box = labelStyle;
+        GUI.skin.button.fontSize = fontsize;
+        GUI.skin.textField.fontSize = fontsize;
+        GUI.skin.textField.alignment = TextAnchor.MiddleLeft;
+        GUI.skin.label.fontSize = fontsize;
+        GUI.skin.label.alignment = TextAnchor.MiddleLeft;
+        //   GUI.Label(new Rect(centerX - 50, centerY - buttonHeight - 10, buttonHeight * 3, buttonHeight), "Bonjour Client");
+
+        GUI.Label(new Rect(centerX - buttonHeight * 4, startY, buttonHeight * 5, buttonHeight - 10), broadcastStatus);
+
+        if (!querying && GUI.Button(new Rect(centerX + buttonHeight * 2 - 10, startY, buttonHeight * 3, buttonHeight - 10), "Broadcast"))
+        {
+            StartBroadcastService();
+        }
+
+        service = GUI.TextField(new Rect(centerX - buttonHeight * 4, startY + buttonHeight, buttonHeight * 5, buttonHeight - 10), service);
+
+        if (!querying && GUI.Button(new Rect(centerX + buttonHeight * 2, startY + buttonHeight, buttonHeight * 3, buttonHeight - 10), "Query"))
+        {
+            StartLookup();
+
+        }
+        if (querying)
+        {
+            // Query status only every 10th frame. Managed -> Native calls are quite expensive.
+            // Similar coding pattern could be considered as good practice. 
+            if (Time.frameCount % 10 == 0)
+            {
+                status = Bonjour.GetLookupStatus();
+                services = Bonjour.GetServiceNames();
+                label = status;
+            }
+
+            if (status == "Done")
+                querying = false;
+
+            //Stop lookup
+            if (querying && GUI.Button(new Rect(centerX + buttonHeight * 2, startY + buttonHeight, buttonHeight * 3, buttonHeight - 10), "Stop"))
+                Bonjour.StopLookup();
+
+        }
+
+        // Display status
+        GUI.Label(new Rect(centerX - buttonHeight * 2, startY + buttonHeight * 2, buttonHeight * 5, buttonHeight - 10), label);
+
+        // List of looked up services
+        for (int i = 0; i < services.Length; i++)
+        {
+            if (services[i] != GetLocalIPv4())
+            {
+                manager.networkAddress = services[i];
+                GUI.Button(new Rect(centerX - buttonHeight * 2, startY + buttonHeight * 3 + i * buttonHeight, buttonHeight * 5, buttonHeight - 10), manager.networkAddress);
+
+                if (manager.networkAddress != "localhost")
+                {
+                    Debug.Log("start client " + manager.networkAddress);
+                    manager.StartClient();
+                    //  this.gameObject.SetActive(false);
+                }
+
+            }
+        }
+    }
+}
