8defe99d3ba47c023eb1806a7ed2372d8213e122
franklinr <franklinr@users.noreply.github.com>
2021-07-03 23:13:20 +0900

    Update BuildPostProcessor.cs

--- Assets/Editor/BuildPostProcessor.cs
+++ Assets/Editor/BuildPostProcessor.cs
@@ -46,8 +46,6 @@ public class BuildPostProcessor
 
         if (buildTarget == BuildTarget.iOS)
         {
-            PlayerSettings.iOS.appleDeveloperTeamID = "3YX4458F9X";
-
             string plistPath = path + "/Info.plist";
             PlistDocument plist = new PlistDocument();
             plist.ReadFromFile(plistPath);
@@ -72,53 +70,4 @@ public class BuildPostProcessor
 
         }
     }
-
-    /*   [PostProcessBuildAttribute(1)]
-       public static void OnPostProcessBuild(BuildTarget target, string path)
-       {
-
-           if (target == BuildTarget.iOS)
-           {
-               PBXProject project = new PBXProject();
-               string sPath = PBXProject.GetPBXProjectPath(path);
-               project.ReadFromFile(sPath);
-
-               // string g = project.GetUnityFrameworkTargetGuid();
-
-               // ModifyFrameworksSettings(project, g);
-
-               // modify frameworks and settings as desired
-               File.WriteAllText(sPath, project.WriteToString());
-           }
-       }
-   */
-    /*   static void ModifyFrameworksSettings(PBXProject project, string g)
-       {
-
-           // add hella frameworks
-
-           Debug.Log(">> Automation, Frameworks... <<");
-
-           project.AddFrameworkToProject(g, "blah.framework", false);
-           project.AddFrameworkToProject(g, "libz.tbd", false);
-
-           // go insane with build settings
-
-           Debug.Log(">> Automation, Settings... <<");
-
-           project.AddBuildProperty(g,
-               "LIBRARY_SEARCH_PATHS",
-               "../blahblah/lib");
-
-           project.AddBuildProperty(g,
-               "OTHER_LDFLAGS",
-               "-lsblah -lbz2");
-
-           // note that, due to some Apple shoddyness, you usually need to turn this off
-           // to allow the project to ARCHIVE correctly (ie, when sending to testflight):
-           project.AddBuildProperty(g,
-               "ENABLE_BITCODE",
-               "false");
-       }*/
-
 }
